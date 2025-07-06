c43f4f64165b9040d53d8b34a533089868a32de5
Артём <fedor.artem.ivanov@mail.ru>
2025-07-06 19:03:56 +0700

    feature(add-second-tree): displaying the second tree

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,24 +1,41 @@
-import {Component, Input, signal, TemplateRef} from '@angular/core';
-import {TreeNode} from '../tree-node';
+import {Component, Input, signal, Signal, TemplateRef, WritableSignal} from '@angular/core';
+import { TreeNode } from '../tree-node';
+import { NgIf, NgForOf, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-tree',
-  standalone: false,
   templateUrl: './tree.component.html',
-  styleUrl: './tree.component.scss'
+  styleUrl: './tree.component.scss',
+  imports: [NgIf, NgForOf, NgTemplateOutlet],
+  standalone: true
 })
 export class TreeComponent {
   @Input() nodes: TreeNode[] = [];
   @Input() template!: TemplateRef<any>;
-  expandedNodes = signal<Set<number>>(new Set());
+  @Input() expandedNodes?: WritableSignal<Set<number>>;
+
+  private localExpanded = signal<Set<number>>(new Set());
+
+  get effectiveExpanded(): Signal<Set<number>> {
+    return this.expandedNodes ?? this.localExpanded;
+  }
 
   toggle(node: TreeNode) {
-    const expanded = this.expandedNodes();
+    const expanded = new Set(this.effectiveExpanded());
     if (expanded.has(node.id)) {
       expanded.delete(node.id);
     } else {
       expanded.add(node.id);
     }
-    this.expandedNodes.set(new Set(expanded));
+
+    if (this.expandedNodes) {
+      this.expandedNodes.set(expanded);
+    } else {
+      this.localExpanded.set(expanded);
+    }
+  }
+
+  isExpanded(node: TreeNode): boolean {
+    return this.effectiveExpanded().has(node.id);
   }
 }
