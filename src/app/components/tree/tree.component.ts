a64424c72b61e8c2da31750cedc7b86e4aad1e82
Артём <fedor.artem.ivanov@mail.ru>
2025-07-07 14:49:26 +0700

    chore: minor change

--- src/app/components/tree/tree.component.ts
+++ src/app/components/tree/tree.component.ts
@@ -1,41 +1,41 @@
 import {Component, Input, signal, Signal, TemplateRef, WritableSignal} from '@angular/core';
-import { TreeNode } from '../../models/tree-node';
-import { NgIf, NgForOf, NgTemplateOutlet } from '@angular/common';
+import {TreeNode} from '../../models/tree-node';
+import {NgIf, NgForOf, NgTemplateOutlet} from '@angular/common';
 
 @Component({
-  selector: 'app-tree',
-  templateUrl: './tree.component.html',
-  styleUrl: './tree.component.scss',
-  imports: [NgIf, NgForOf, NgTemplateOutlet],
-  standalone: true
+    selector: 'app-tree',
+    templateUrl: './tree.component.html',
+    styleUrl: './tree.component.scss',
+    imports: [NgIf, NgForOf, NgTemplateOutlet],
+    standalone: true
 })
 export class TreeComponent {
-  @Input() nodes: TreeNode[] = [];
-  @Input() template!: TemplateRef<any>;
-  @Input() expandedNodes?: WritableSignal<Set<number>>;
+    @Input() nodes: TreeNode[] = [];
+    @Input() template!: TemplateRef<any>;
+    @Input() expandedNodes?: WritableSignal<Set<number>>;
 
-  private localExpanded = signal<Set<number>>(new Set());
+    private localExpanded = signal<Set<number>>(new Set());
 
-  get effectiveExpanded(): Signal<Set<number>> {
-    return this.expandedNodes ?? this.localExpanded;
-  }
-
-  toggle(node: TreeNode) {
-    const expanded = new Set(this.effectiveExpanded());
-    if (expanded.has(node.id)) {
-      expanded.delete(node.id);
-    } else {
-      expanded.add(node.id);
+    get effectiveExpanded(): Signal<Set<number>> {
+        return this.expandedNodes ?? this.localExpanded;
     }
 
-    if (this.expandedNodes) {
-      this.expandedNodes.set(expanded);
-    } else {
-      this.localExpanded.set(expanded);
-    }
-  }
+    toggle(node: TreeNode) {
+        const expanded = new Set(this.effectiveExpanded());
+        if (expanded.has(node.id)) {
+            expanded.delete(node.id);
+        } else {
+            expanded.add(node.id);
+        }
 
-  isExpanded(node: TreeNode): boolean {
-    return this.effectiveExpanded().has(node.id);
-  }
+        if (this.expandedNodes) {
+            this.expandedNodes.set(expanded);
+        } else {
+            this.localExpanded.set(expanded);
+        }
+    }
+
+    isExpanded(node: TreeNode): boolean {
+        return this.effectiveExpanded().has(node.id);
+    }
 }
--- src/app/services/tree-data.service.ts
+++ src/app/services/tree-data.service.ts
@@ -1,33 +1,33 @@
-import { Injectable, signal } from '@angular/core';
-import { DEFAULT_TREE_NODES, TreeNode } from '../models/tree-node';
+import {Injectable, signal} from '@angular/core';
+import {DEFAULT_TREE_NODES, TreeNode} from '../models/tree-node';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class TreeDataService {
-  private treeNodes = signal<TreeNode[]>(DEFAULT_TREE_NODES);
+    private treeNodes = signal<TreeNode[]>(DEFAULT_TREE_NODES);
 
-  expandedNodes = signal<Set<number>>(new Set());
+    expandedNodes = signal<Set<number>>(new Set());
 
-  getNodes = this.treeNodes.asReadonly();
+    getNodes = this.treeNodes.asReadonly();
 
-  logNode(node: TreeNode) {
-    console.log(`ID узла: ${node.id}`);
-  }
+    logNode(node: TreeNode) {
+        console.log(`ID узла: ${node.id}`);
+    }
 
-  expandAll(node: TreeNode) {
-    const expandRecursive = (n: TreeNode) => {
-      this.expandedNodes.update(set => {
-        const newSet = new Set(set);
-        newSet.add(n.id);
-        return newSet;
-      });
+    expandAll(node: TreeNode) {
+        const expandRecursive = (n: TreeNode) => {
+            this.expandedNodes.update(set => {
+                const newSet = new Set(set);
+                newSet.add(n.id);
+                return newSet;
+            });
 
-      if (n.children?.length) {
-        n.children.forEach(expandRecursive);
-      }
-    };
+            if (n.children?.length) {
+                n.children.forEach(expandRecursive);
+            }
+        };
 
-    expandRecursive(node);
-  }
+        expandRecursive(node);
+    }
 }
